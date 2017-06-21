68141992ab78ab38d9d90b9d83834c3bdd3eda85
Jonas Jansson <jonas@Jonass-MacBook-Pro.local>
2017-06-21 16:54:05 +0200

    afsd

--- js/script.js
+++ js/script.js
@@ -14,43 +14,13 @@ $(window).load(function() {
 })
 
 $(document).ready(function() {
-		//animated logo
-		$(".navbar-brand").hover(function () {
-			$(this).toggleClass("animated shake");
-		});
-		
-		//animated scroll_arrow
-		$(".img_scroll").hover(function () {
-			$(this).toggleClass("animated infinite bounce");
-		});
+
+
 		
 		//Wow Animation DISABLE FOR ANIMATION MOBILE/TABLET
-		wow = new WOW(
-		{
-			mobile: false
-		});
-		wow.init();
-		
-		//MagnificPopup
-		$('.image-link').magnificPopup({type:'image'});
-
 
 		// OwlCarousel N1
-		$("#owl-demo").owlCarousel({
-			autoPlay: 3000,
-			items : 3,
-			itemsDesktop : [1199,3],
-			itemsDesktopSmall : [979,3]
-		});
-
-		// OwlCarousel N2
-		$("#owl-demo-1").owlCarousel({
-			  navigation : false, // Show next and prev buttons
-			  slideSpeed : 300,
-			  paginationSpeed : 400,
-			  singleItem:true
-		});
-
+/*
 		//SmothScroll
 		$('a[href*=#]').click(function() {
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
@@ -65,6 +35,6 @@ $(document).ready(function() {
 			}
 		});
 		
-
+*/
 });
 
